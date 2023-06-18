52132f8d96f5eac9531ecfcc03413ca8cd6f83d2
murasaqi <uma9266@gmail.com>
2023-06-19 00:13:19 +0900

    TimeMachineのTimelineAssetを自動生成するScriptを追加

---
+++ jp.iridescent.timemachine/Runtime/TimeMachinePlayer.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+using Iridescent.TimeMachine;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Serialization;
+using UnityEngine.Timeline;
+
+public class TimeMachinePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TimeMachineTrackManager timeMachineTrackManager;
+    public List<PlayableDirector> playableDirectors = new List<PlayableDirector>();
+    public bool exportTimelineAsset = false;
+    public string exportPath = "Assets/TimeMachineTimeline.asset";
+   
+    #if UNITY_EDITOR
+    public TimelineAsset timelineAsset;
+    [FormerlySerializedAs("playableDirector")] public PlayableDirector timeMachinePlayableDirector;
+    #endif
+    void Start()
+    {
+        
+    }
+
+    [ContextMenu("Create TimelineAsset")]
+    public void CreateTimelineAsset()
+    {
+        if (timeMachinePlayableDirector == null)
+        {
+            timeMachinePlayableDirector = GetComponent<PlayableDirector>();
+            if (timeMachinePlayableDirector == null) timeMachinePlayableDirector = gameObject.AddComponent<PlayableDirector>();
+        }
+        ReleaseTimelineAsset();
+        timelineAsset = ScriptableObject.CreateInstance<TimelineAsset>();
+        timelineAsset.name = "TimeMachineTimeline";
+        timelineAsset.editorSettings.fps = 60;
+        // save asset and export timelineAsset
+
+#if UNITY_EDITOR
+        if (exportTimelineAsset)
+        {
+            ExportTimelineAsset();
+            // LoadTimelineAsset();
+            
+        }
+#endif
+        
+        
+        CreateTrackAndClips();
+
+
+
+        
+    }
+    
+    public void ExportTimelineAsset()
+    {
+        if (timelineAsset == null) return;
+        UnityEditor.AssetDatabase.CreateAsset(timelineAsset, exportPath);
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.AssetDatabase.Refresh();
+        
+        
+        
+    }
+    
+    public void LoadTimelineAsset()
+    {
+        ReleaseTimelineAsset();
+        timelineAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<TimelineAsset>(exportPath);
+        if (timelineAsset == null)
+        {
+            Debug.LogError("TimelineAsset is not found");
+            return;
+        }
+        CreateTrackAndClips();
+    }
+
+    public void CreateTrackAndClips()
+    {
+        double startTime = 0f;
+        var controlTrack = timelineAsset.CreateTrack<ControlTrack>(null, "ControlTrack");
+        var timeMachineTrack = timelineAsset.CreateTrack<TimeMachineControlTrack>(null, "TimeMachineTrack");
+        // set binding
+        if(timeMachineTrackManager == null) return;
+        timeMachinePlayableDirector.SetGenericBinding(timeMachineTrack, timeMachineTrackManager);
+        // make ControlTrack and add it to control clip
+        foreach (var playableDirector in playableDirectors)
+        {
+            if(playableDirector == null)continue;
+            var controlClip = controlTrack.CreateDefaultClip();
+            controlClip.displayName = playableDirector.playableAsset.name;
+            controlClip.duration = playableDirector.duration;
+            controlClip.start = startTime;
+            controlClip.timeScale = 1;
+            controlClip.blendInDuration = 0;
+            controlClip.blendOutDuration = 0;
+            controlClip.easeInDuration = 0;
+            controlClip.easeOutDuration = 0;
+            var controlPlayableAsset = controlClip.asset as ControlPlayableAsset;
+            controlPlayableAsset.sourceGameObject = new ExposedReference<GameObject>()
+            {
+                defaultValue = playableDirector.gameObject,
+                exposedName = playableDirector.gameObject.name
+            };
+            
+            var timeMachineClip = timeMachineTrack.CreateDefaultClip();
+            timeMachineClip.displayName = playableDirector.playableAsset.name;
+            timeMachineClip.duration = playableDirector.duration;
+            timeMachineClip.start = startTime;
+            
+            startTime += playableDirector.duration;
+        }
+        
+        timeMachinePlayableDirector.playableAsset = timelineAsset;
+        
+        //set dirty
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.SetDirty(timelineAsset);
+        UnityEditor.EditorUtility.SetDirty(timeMachinePlayableDirector);
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.AssetDatabase.Refresh();
+#endif
+
+    }
+    
+    
+    
+    public void ReleaseTimelineAsset()
+    {
+        if (timelineAsset != null)
+        {
+            DestroyImmediate(timelineAsset);
+        }
+        timelineAsset = null;
+    }
+
+    private void OnDestroy()
+    {
+        ReleaseTimelineAsset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
