ab692b0aaca6e0553dc5324873ce5f241dada6c3
murasaqi <murasaqi@iridescent.jp>
2024-10-21 03:23:06 +0900

    自動生成システムがCustomControlTrackに対応

--- jp.iridescent.timemachine/Runtime/TimeMachinePlayer.cs
+++ jp.iridescent.timemachine/Runtime/TimeMachinePlayer.cs
@@ -1,24 +1,27 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Serialization;
 using UnityEngine.Timeline;
-
+#if USE_CUSTOMCONTROLTRACK
+using Iridescent.Timeline;
+#endif
 
 namespace Iridescent.TimeMachine
 {
-    [RequireComponent(typeof(PlayableDirector))]
+    // [RequireComponent(typeof(PlayableDirector))]
     public class TimeMachinePlayer : MonoBehaviour
     {
         // Start is called before the first frame update
         public TimeMachineTrackManager timeMachineTrackManager;
         public List<PlayableDirector> playableDirectors = new List<PlayableDirector>();
-        public bool exportTimelineAsset = false;
+        // public bool exportTimelineAsset = false;
         public string exportPath = "Assets/TimeMachineTimeline.asset";
         
         public TimelineAsset timelineAsset;
-        [FormerlySerializedAs("playableDirector")]
-        public PlayableDirector timeMachinePlayableDirector;
+        // [FormerlySerializedAs("playableDirector")]
+        // public PlayableDirector timeMachinePlayableDirector;
 
         void Start()
         {
@@ -28,27 +31,29 @@ namespace Iridescent.TimeMachine
         [ContextMenu("Create TimelineAsset")]
         public void CreateTimelineAsset()
         {
-            if (timeMachinePlayableDirector == null)
-            {
-                timeMachinePlayableDirector = GetComponent<PlayableDirector>();
-                if (timeMachinePlayableDirector == null)
-                    timeMachinePlayableDirector = gameObject.AddComponent<PlayableDirector>();
-            }
+            // if (timeMachinePlayableDirector == null)
+            // {
+            //     timeMachinePlayableDirector = GetComponent<PlayableDirector>();
+            //     if (timeMachinePlayableDirector == null)
+            //         timeMachinePlayableDirector = gameObject.AddComponent<PlayableDirector>();
+            // }
 
-            ReleaseTimelineAsset();
-            timelineAsset = ScriptableObject.CreateInstance<TimelineAsset>();
-            timelineAsset.name = "TimeMachineTimeline";
-            timelineAsset.editorSettings.fps = 60;
+            // ReleaseTimelineAsset();
+            timelineAsset = timeMachineTrackManager.playableDirector.playableAsset as TimelineAsset;
+            // timelineAsset.name = "TimeMachineTimeline";
+            // timelineAsset.editorSettings.fps = 60;
             // save asset and export timelineAsset
+            
+            // new PlayableDirector
 
-#if UNITY_EDITOR
-            if (exportTimelineAsset)
-            {
-                ExportTimelineAsset();
-                // LoadTimelineAsset();
-
-            }
-#endif
+// #if UNITY_EDITOR
+//             if (exportTimelineAsset)
+//             {
+//                 ExportTimelineAsset();
+//                 // LoadTimelineAsset();
+//
+//             }
+// #endif
 
 
             CreateTrackAndClips();
@@ -85,10 +90,24 @@ namespace Iridescent.TimeMachine
         public void CreateTrackAndClips()
         {
             double startTime = 0f;
-            var controlTrack = timelineAsset.CreateTrack<ControlTrack>(null, "ControlTrack");
-            var timeMachineTrack = timelineAsset.CreateTrack<TimeMachineControlTrack>(null, "TimeMachineTrack");
+            var tracks = timelineAsset.GetOutputTracks().ToList();
+            
+#if USE_CUSTOMCONTROLTRACK
+            // search control track int timelineAsset
+            // var controlTrack = tracks.Find(track => track.GetType() == typeof(CustomControlTrack)) as CustomControlTrack;
+            var controlTrack = timelineAsset.CreateTrack<CustomControlTrack>(null, "ControlTrack");
+#else
+            var controlTrack = tracks.Find(track => track.GetType() == typeof(ControlTrack)) as ControlTrack;
+            if(controlTrack == null)timelineAsset.CreateTrack<ControlTrack>(null, "ControlTrack");
+#endif
+            
+            var timeMachineTrack = tracks.Find(track => track.GetType() == typeof(TimeMachineControlTrack)) as TimeMachineControlTrack;
+            if(timeMachineTrack == null) timeMachineTrack = timelineAsset.CreateTrack<TimeMachineControlTrack>(null, "TimeMachineTrack");
+            
+            
             // set binding
             if (timeMachineTrackManager == null) return;
+            var timeMachinePlayableDirector = timeMachineTrackManager.playableDirector;
             timeMachinePlayableDirector.SetGenericBinding(timeMachineTrack, timeMachineTrackManager);
             // make ControlTrack and add it to control clip
             foreach (var playableDirector in playableDirectors)
@@ -103,11 +122,15 @@ namespace Iridescent.TimeMachine
                 controlClip.blendOutDuration = 0;
                 controlClip.easeInDuration = 0;
                 controlClip.easeOutDuration = 0;
+#if USE_CUSTOMCONTROLTRACK
+                var controlPlayableAsset = controlClip.asset as CustomControlPlayableAsset;
+#else
                 var controlPlayableAsset = controlClip.asset as ControlPlayableAsset;
+#endif
                 controlPlayableAsset.sourceGameObject = new ExposedReference<GameObject>()
                 {
-                    defaultValue = playableDirector.gameObject,
-                    exposedName = playableDirector.gameObject.name
+                    defaultValue = playableDirector,
+                    exposedName = playableDirector.name
                 };
 
                 var timeMachineClip = timeMachineTrack.CreateDefaultClip();
